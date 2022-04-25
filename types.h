ff228d6b34e480662bb9b0839e5aec174b2f6f08
Forprix <81227518+Forprix@users.noreply.github.com>
2022-04-25 16:00:45 +0300

    Add files via upload

---
+++ types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+
+typedef char int8;
+typedef short int16;
+typedef long int32;
+typedef long long int64;
+
+typedef wchar_t wchar;
+
+typedef char* str;
+typedef wchar_t* wcs;
+typedef const char* cstr;
+typedef const wchar_t* cwcs;
